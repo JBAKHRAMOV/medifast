e4bc3cac214ec7f14539b7d1334ea4db3e384d55
Akobir <akobir.abduganiev@ya.ru>
2022-06-23 23:12:01 +0500

    Main class modified!

--- src/main/java/com/company/MediFastApplication.java
+++ src/main/java/com/company/MediFastApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class DemoApplication {
+public class MediFastApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(MediFastApplication.class, args);
     }
 
 }
