06a30dea2c0363199d204ac6a1065bb8a0513690
Akobir <akobir.abduganiev@ya.ru>
2022-07-03 01:39:35 +0500

    Pdf Section Added

--- src/test/java/com/company/MediFastApplicationTests.java
+++ src/test/java/com/company/MediFastApplicationTests.java
@@ -1,13 +1,33 @@
 package com.company;
 
+import com.company.entity.BotUsersEntity;
+import com.company.enums.Gender;
+import com.company.service.PdfGeneratorService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 class MediFastApplicationTests {
 
     @Test
     void contextLoads() {
+        BotUsersEntity botUsersEntity = new BotUsersEntity();
+        botUsersEntity.setTelegramId(123456789L);
+        botUsersEntity.setName("Ali");
+        botUsersEntity.setSurname("Valiyev");
+        botUsersEntity.setPhone("+998932158000");
+        botUsersEntity.setBirthDate(LocalDate.now());
+        botUsersEntity.setHeight("170-sm");
+        botUsersEntity.setGender(Gender.MALE);
+        botUsersEntity.setWeight("90-kg");
+        botUsersEntity.setCurrentTemperature(40D);
+        PdfGeneratorService pdfGeneratorService = new PdfGeneratorService();
+        pdfGeneratorService.export(botUsersEntity);
     }
+
 
 }
