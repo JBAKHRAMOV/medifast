5d66b07736f36c5717500f067aaa7e990a9d179f
Akobir <akobir.abduganiev@ya.ru>
2022-08-07 23:50:41 +0500

    CorsConfig added

--- src/main/java/com/company/api/config/security/WebSecurityConfig.java
+++ src/main/java/com/company/api/config/security/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @AllArgsConstructor
@@ -44,6 +45,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.cors()
+                .configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
         http.addFilterBefore(jwtTokenFilter,
                 UsernamePasswordAuthenticationFilter.class);
         http.authorizeRequests()
